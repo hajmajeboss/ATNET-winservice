5991b2ea4a87710187dbe4943d4f3f55fc1b8d71
Petr Heinz <p.heinz@email.cz>
2018-12-30 23:27:25 +0100

    sndr

--- TestConsoleAPp/MailSender.cs
+++ TestConsoleAPp/MailSender.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,21 @@ namespace CryptoSerice
 {
     public class MailSender
     {
+        public void SendGraph(string to, string path)
+        {
+            SmtpClient client = new SmtpClient();
+            Console.WriteLine("client set");
+            MailMessage message = new MailMessage(new MailAddress("p.heinz@email.cz", "Petr Heinz"), new MailAddress(to));
+            message.Subject = "CryptoApp - Bitcoin price list for the day";
+            message.Attachments.Add(new Attachment(path));
+            //await client.SendAsync(message);
+            Console.WriteLine("message set");
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Host = "smtp.seznam.cz";
+            client.Credentials = new System.Net.NetworkCredential("username", "password");
+            client.Send(message);
+            Console.WriteLine("Sent.");
+        }
     }
 }
--- TestConsoleAPp/Program.cs
+++ TestConsoleAPp/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestConsoleAPp
@@ -15,6 +16,13 @@ namespace TestConsoleAPp
             var btc = downloader.DownloadData();
             var graphProvider = new BitcoinGraphProvider();
             graphProvider.GenerateBitcoinGraph(btc, "graphs");
+
+            var mailSender = new MailSender();
+
+            Thread t = new Thread(() => { mailSender.SendGraph("p.heinz@email.cz", "graphs.png"); });
+            t.Start();
+            Console.WriteLine("Sending.");
+            t.Join();
         }
     }
 }
